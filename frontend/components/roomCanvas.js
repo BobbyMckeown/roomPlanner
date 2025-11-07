49aa0a77098d502dd8ea19768c331773c84e5614
bobby <mckeownbobby4@gmail.com>
2025-11-07 12:16:32 +0000

    Corrected the correct src for js and now displays a red circle

--- frontend/components/roomCanvas.js
+++ frontend/components/roomCanvas.js
@@ -1,12 +1,12 @@
-var stage = new konva.Stage({ //creating a konva stage
+var stage = new Konva.Stage({ //creating a konva stage
     container: "konva-holder", 
-    Width: window.innerWidth,  
+    width: window.innerWidth,  
     height: window.innerHeight,
 });
 
-var layer = new konva.Layer(); //creating a konva layer
+var layer = new Konva.Layer(); //creating a konva layer
 
-var circle = new konva.circle({
+var circle = new Konva.Circle({
     x: stage.width() / 2,
     y: stage.height() / 2,
     radius: 70,
